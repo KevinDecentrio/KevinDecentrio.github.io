2112be22bad66086bf9656245dea4c2044a681fd
Kevin Kaslana <kevin@decentrio.ventures>
2024-07-15 22:24:31 +0200

    Fix wrong release commit

--- src/components/swap/auction-list/helpers.ts
+++
@@ -1,8 +0,0 @@
-import { AuctionInfo } from '../../../fetchers/auction-infos';
-
-export const byStartHeightAscending = (a: AuctionInfo, b: AuctionInfo) => {
-  if (!a.auction.description?.startHeight || !b.auction.description?.startHeight) {
-    return 0;
-  }
-  return Number(a.auction.description.startHeight - b.auction.description.startHeight);
-};
